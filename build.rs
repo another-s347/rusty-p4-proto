40d93bd1ed362e850a355570d8c4fc75885f8a44
skye347 <s347419313@gmail.com>
2021-02-13 22:58:14 +0800

    update p4runtime api to v1.3.0 & prost 0.7 tonic 0.4

--- build.rs
+++ build.rs
@@ -1,19 +1,25 @@
 fn main() {
+    let mut config = prost_build::Config::new();
+    config.bytes(&["."]);
     tonic_build::configure()
         .build_server(false)
-        .compile(
-        &[
-            "./p4runtime/proto/p4/v1/p4runtime.proto",
-            "./p4runtime/proto/p4/v1/p4data.proto",
-            "./p4runtime/proto/p4/config/v1/p4info.proto",
-            "./p4runtime/proto/p4/config/v1/p4types.proto",
-            "./googleapis/google/rpc/status.proto",
-            "./googleapis/google/rpc/code.proto"],
-        &["./p4runtime/proto/","./googleapis/"]
+        .compile_with_config(
+            config,
+            &[
+                "./p4runtime/proto/p4/v1/p4runtime.proto",
+                "./p4runtime/proto/p4/v1/p4data.proto",
+                "./p4runtime/proto/p4/config/v1/p4info.proto",
+                "./p4runtime/proto/p4/config/v1/p4types.proto",
+                "./googleapis/google/rpc/status.proto",
+                "./googleapis/google/rpc/code.proto"],
+            &["./p4runtime/proto/","./googleapis/"]
         ).unwrap();
+    let mut config = prost_build::Config::new();
+    config.bytes(&["."]);
     tonic_build::configure()
         .build_server(false)
-        .compile(
+        .compile_with_config(
+            config,
             &[
                 "./gnmi/gnmi.proto",
                 "./gnmi/gnmi_ext.proto"],
